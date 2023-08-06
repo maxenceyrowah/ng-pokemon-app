2ed5d5f45091856d35f5ad6783d2e046fc6564c9
maxence_yrowah <kouame14@gmail.com>
2023-08-06 02:57:43 +0000

    fix architecture of app by module

--- src/app/pokemon/detail-pokemon/detail-pokemon.component.ts
+++ src/app/pokemon/detail-pokemon/detail-pokemon.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { Pokemon } from '../pokemon';
 import { POKEMONS } from '../mock-pokemon-list';
@@ -12,7 +12,7 @@ export class DetailPokemonComponent implements OnInit {
   pokemonList: Pokemon[];
   pokemon: Pokemon | undefined;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit() {
     this.pokemonList = POKEMONS;
@@ -21,5 +21,9 @@ export class DetailPokemonComponent implements OnInit {
     if (pokemonId) {
       this.pokemon = this.pokemonList.find(pokemon => pokemon.id === +pokemonId);
     }
+  }
+
+  goToPokemonList() {
+    this.router.navigate(['/pokemons']);
   }
 }
