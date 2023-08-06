a33e4c89bf9d15c9eff5b76418b3ba929ed57673
maxence_yrowah <kouame14@gmail.com>
2023-08-06 02:05:40 +0000

    init ng-pokemon-app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ListPokemonComponent } from './list-pokemon/list-pokemon.component';
+import { DetailPokemonComponent } from './detail-pokemon/detail-pokemon.component';
+
+const routes: Routes = [
+  { path: 'pokemons', component: ListPokemonComponent },
+  { path: 'pokemon/:id', component: DetailPokemonComponent },
+  { path: '', redirectTo: 'pokemons', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/border-card.directive.ts
@@ -0,0 +1,34 @@
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+
+@Directive({
+  selector: '[pkmnBorderCard]'
+})
+export class BorderCardDirective {
+  private initialColor: string = "#f5f5f5";
+  private defaultColorr: string = "#009688";
+  private defaultHeight: number = 180;
+  
+  @Input('pkmnBorderCard') borderColor: string;
+
+  constructor(private refElement: ElementRef) {
+    this.setBorder(this.initialColor);
+    this.setHeight(this.defaultHeight);
+  }
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.setBorder(this.borderColor || this.defaultColorr);
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.setBorder(this.initialColor);
+  }
+
+  private setHeight(height: number) {
+    this.refElement.nativeElement.style.height = `${height}px`;
+  }
+
+  private setBorder(color: string) {
+    this.refElement.nativeElement.style.border = `solid 4px ${color}`;
+  }
+
+}
